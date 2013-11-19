81274fd451cd6da703e44c36e4546b0dce2582b8
Szymon <szymon.tonderys@nsn.com>
2013-11-19 16:30:00 +0100

    working fine, needs refactor

--- Draw_a_matrix.py
+++ Draw_a_matrix.py
@@ -3,14 +3,17 @@ from time import *
 import socket
 from threading import Thread
 
+WIDTH = 640
+HEIGHT = 640
+
 class Draw_a_matrix:
     TCP_IP = 'localhost'
     TCP_PORT = 5005
-    buffer_size = 1024 
+    buffer_size = 4096 
     
     def __init__(self):
         self.top_window = Tk()
-        self.canvas = Canvas(self.top_window, width = "640", height = "640")
+        self.canvas = Canvas(self.top_window, width = WIDTH, height = HEIGHT)
         self.canvas.grid(row = 0, column = 0)
         self.drawer = self.Drawing_thread(self)
         try:
@@ -21,7 +24,6 @@ class Draw_a_matrix:
         self.drawer.start()
         self.sock.bind((self.TCP_IP, self.TCP_PORT))
         self.sock.listen(5)
-        print "nathin'"
 
     class Drawing_thread(Thread):
         def __init__(self, controller):
@@ -36,14 +38,19 @@ class Draw_a_matrix:
                 parsed_data = self.controller.parse_list_from_string(data)
                 self.columns = parsed_data[0]
                 self.rows = parsed_data[1] 
-                for i in range (0,self.rows+1):
-                    for j in range (0, self.columns+1):
-                        if parsed_data[i*10+j+2] == 0:
-                            self.controller.canvas.create_rectangle(i*20,j*20,i*21,j*21, fill="white")
-                        if parsed_data[i+j+2] == 1:
-                            self.controller.canvas.create_rectangle(i*20,j*20,i*21,j*21, fill="black")
-                        if parsed_data[i+j+2] == 2:
-                            self.controller.canvas.create_rectangle(i*20,j*20,i*21,j*21, fill="green")
+                pixel_width = WIDTH / self.columns
+                pixel_height = HEIGHT / self.rows
+                for y in range (0, self.rows):
+                    for x in range (0, self.columns):
+                        if parsed_data[y * self.columns + x + 2] == 0:
+                            color = 'white'
+                        if parsed_data[y * self.columns + x + 2] == 1:
+                            color = 'black'
+                        if parsed_data[y * self.columns + x + 2] == 2:
+                            color = 'green'
+                        Y = y * pixel_height
+                        X = x * pixel_width
+                        self.controller.canvas.create_rectangle(Y, X, Y + pixel_height, X + pixel_width, fill=color)
                 conn.close()    
 
 
--- sender.py
+++ sender.py
@@ -1,4 +1,5 @@
 import socket
+import random
 from threading import Thread
 
 class Matrix_sender:
@@ -6,18 +7,15 @@ class Matrix_sender:
    TCP_PORT = 5005
 
    def __init__(self):
+        rows = 32
+        columns = 32
+        message = "%d %d "%(rows, columns)
+        for i in range(0, rows):
+            for j in  range(0, columns):
+                message = message + "%d "%random.randrange(0,3,1)
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.TCP_IP, self.TCP_PORT))
-        self.s.send("10 10\n"
-        "2 1 0 0 0 0 0 0 0 0\n"
-        "1 2 1 0 0 0 0 0 0 0\n"
-        "0 1 2 1 0 0 0 0 0 0\n"
-        "0 0 1 2 1 0 0 0 0 0\n"
-        "0 0 0 1 2 1 0 0 0 0\n"
-        "0 0 0 0 1 2 1 0 0 0\n"
-        "0 0 0 0 0 1 2 1 0 0\n"
-        "0 0 0 0 0 0 1 2 1 0\n"
-        "0 0 0 0 0 0 0 1 2 1\n"
-        "0 0 0 0 0 0 0 0 1 2\n")
+        self.s.send(message)
+        print message
         self.s.close()
 Matrix_sender()
